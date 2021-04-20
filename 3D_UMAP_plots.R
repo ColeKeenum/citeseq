00617578013da63ad5c4bc98985da6e396cf97f6
Cole Keenum <mkeenum3@gatech.edu>
2021-04-19 22:28:34 -0400

    updating 3D UMAP

--- 3D_UMAP_plots.R
+++ 3D_UMAP_plots.R
@@ -69,33 +69,37 @@ plot_ly(data = plot.data,
 # WORKSPACE SAVED 04/19/2021 ~5PM APPROX
 
 neutro <- RunUMAP(neutro, reduction = 'pca', dims = 1:30,
-                  assay = 'integratedSCT_', reduction.name = 'sct.3d',
-                  reduction.key = 'sct3d_', n.components = 3L)
+                  assay = 'integratedSCT_', reduction.name = 'sct.umap',
+                  reduction.key = 'sctUMAP_', n.components = 3L)
 
-umap_1 <- neutro[["sct.3d"]]@cell.embeddings[,1]
-umap_2 <- neutro[["sct.3d"]]@cell.embeddings[,2]
-umap_3 <- neutro[["sct.3d"]]@cell.embeddings[,3]
+umap_1 <- neutro[["sct.umap"]]@cell.embeddings[,1]
+umap_2 <- neutro[["sct.umap"]]@cell.embeddings[,2]
+umap_3 <- neutro[["sct.umap"]]@cell.embeddings[,3]
 
-Embeddings(object = neutro, reduction = "sct.3d")
+head(Embeddings(object = neutro, reduction = "sct.umap"))
 
-plot.data <- FetchData(object = neutro, vars = c("sct3d_1", "sct3d_2", "sct3d_3", "seurat_clusters"))
-plot.data$label <- paste(rownames(plot.data))
+plot.data <- FetchData(object = neutro, vars = c("sctUMAP_1", "sctUMAP_2", "sctUMAP_3", "seurat_clusters"))
+#plot.data$label <- paste(rownames(plot.data))
+plot.data$label <- paste(Idents(neutro))
 
 fig <- plot_ly(data = plot.data, 
-               x = ~sct3d_1, y = ~sct3d_2, z = ~sct3d_3, 
+               x = ~sctUMAP_1, y = ~sctUMAP_2, z = ~sctUMAP_3, 
                color = ~seurat_clusters,
                type = "scatter3d", 
                mode = "markers", 
                marker = list(size = 5, width=2), # controls size of points
                text=~label, #This is that extra column we made earlier for which we will use for cell ID
                hoverinfo="text") #When you visualize your plotly object, hovering your mouse pointer over a point shows cell names
+fig
 
-plot.data <- FetchData(object = neutro, vars = c("sct3d_1", "sct3d_2", "sct3d_3", "celltype"))
-plot.data$label <- paste(rownames(plot.data))
+plot.data <- FetchData(object = neutro, vars = c("sctUMAP_1", "sctUMAP_2", "sctUMAP_3", "celltype"))
+#plot.data$label <- paste(rownames(plot.data))
+plot.data$label <- paste(neutro$celltype)
 
 plot_ly(data = plot.data, 
-        x = ~sct3d_1, y = ~sct3d_2, z = ~sct3d_3, 
+        x = ~sctUMAP_1, y = ~sctUMAP_2, z = ~sctUMAP_3,
         color = ~celltype,
+        colors = color_df$Color, # from main script,
         type = "scatter3d", 
         mode = "markers", 
         marker = list(size = 5, width=2), # controls size of points
@@ -127,20 +131,40 @@ fig
 fig_cube
 
 DefaultAssay(neutro) <- "SCT"
-plot.data <- FetchData(object = neutro, vars = c("sct3d_1", "sct3d_2", "sct3d_3", "Mmp8"), slot = 'data')
+plot.data <- FetchData(object = neutro, vars = c("sctUMAP_1", "sctUMAP_2", "sctUMAP_3", "Mmp8"), slot = 'data')
 
 # First make another column in your dataframe, where all values above 1 are re-assigned a value of 1
 # This information is stored in the 'changed' column of your dataframe
 plot.data$changed <- ifelse(test = plot.data$Mmp8 <1, yes = plot.data$Mmp8, no = 1)
 
 # Add the label column, so that now the column has 'cellname-its expression value'
-plot.data$label <- paste(rownames(plot.data)," - ", plot.data$Mmp8, sep="")
+plot.data$label <- paste(neutro$celltype," - ", plot.data$Mmp8, sep="")
 
 plot_ly(data = plot.data, 
-        x = ~sct3d_1, y = ~sct3d_2, z = ~sct3d_3, 
+        x = ~sctUMAP_1, y = ~sctUMAP_2, z = ~sctUMAP_3,
         color = ~Mmp8, # you can just run this against the column for the gene as well using ~ACTB, the algorith will automatically scale in that case based on maximal and minimal values
         opacity = .5,
-        colors = c('darkgreen', 'red'), 
+        colors = c('darkgrey', 'red'), 
+        type = "scatter3d", 
+        mode = "markers",
+        marker = list(size = 5, width=2), 
+        text=~label,
+        hoverinfo="text")
+
+plot.data <- FetchData(object = neutro, vars = c("sctUMAP_1", "sctUMAP_2", "sctUMAP_3", "Ccl3"), slot = 'data')
+
+# First make another column in your dataframe, where all values above 1 are re-assigned a value of 1
+# This information is stored in the 'changed' column of your dataframe
+plot.data$changed <- ifelse(test = plot.data$Ccl3 <1, yes = plot.data$Ccl3, no = 1)
+
+# Add the label column, so that now the column has 'cellname-its expression value'
+plot.data$label <- paste(neutro$celltype," - ", plot.data$Ccl3, sep="")
+
+plot_ly(data = plot.data, 
+        x = ~sctUMAP_1, y = ~sctUMAP_2, z = ~sctUMAP_3,
+        color = ~Ccl3, # you can just run this against the column for the gene as well using ~ACTB, the algorith will automatically scale in that case based on maximal and minimal values
+        opacity = .5,
+        colors = c('darkgrey', 'blue'), 
         type = "scatter3d", 
         mode = "markers",
         marker = list(size = 5, width=2), 
